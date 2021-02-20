e190329280095b34194f08395aa071dc9ccf4de2
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-02-20 17:18:24 -0500

    stuff

--- src/components/VideoTrack/VideoTrack.tsx
+++ src/components/VideoTrack/VideoTrack.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { IVideoTrack } from '../../types';
 import { styled } from '@material-ui/core/styles';
 import { Track } from 'twilio-video';
 import useMediaStreamTrack from '../../hooks/useMediaStreamTrack/useMediaStreamTrack';
 import useVideoTrackDimensions from '../../hooks/useVideoTrackDimensions/useVideoTrackDimensions';
+import axios from 'axios';
 
 const Video = styled('video')({
   width: '100%',
@@ -29,6 +30,19 @@ export default function VideoTrack({ track, isLocal, priority }: VideoTrackProps
       track.setPriority(priority);
     }
     track.attach(el);
+    const interval = setInterval(() => {
+      const canvas = document.createElement('canvas');
+      canvas.width = 640;
+      canvas.height = 480;
+      //   var ctx = canvas.getContext('2d')
+      //   ctx?.drawImage(ref.current, 0, 0, canvas.width, canvas.height)
+      //   let frame = ctx?.getImageData(0, 0, canvas.width, canvas.height)
+      //   var data  = canvas.toDataURL('image/jpeg')
+
+      //      axios.post('localhost:5000/expression', {imageData: data}).then((response) => {console.log(response.data)})
+
+      // },5000)
+    }, 5000);
     return () => {
       track.detach(el);
       if (track.setPriority && priority) {
@@ -45,5 +59,9 @@ export default function VideoTrack({ track, isLocal, priority }: VideoTrackProps
     objectFit: isPortrait || track.name.includes('screen') ? ('contain' as const) : ('cover' as const),
   };
 
-  return <Video ref={ref} style={style} />;
+  return (
+    <div>
+      <Video ref={ref} style={style} />;
+    </div>
+  );
 }
